a26eb80748a93142da8b23eea8def58fffc5ea32
Duy Tan <167642907+DiTan1606@users.noreply.github.com>
2024-05-23 23:34:59 +0700

    Tân: tạo class dohoa

---
+++ Project_folder/TPDT_SNAKE/dohoa.h
@@ -0,0 +1,29 @@
+#pragma once
+#ifndef RENDERER_H
+#define RENDERER_H
+#include<string>
+#include <vector>
+#include "SDL.h"
+#include "Snake.h"
+#include "SDL_image.h"
+class dohoa {
+public:
+    dohoa(const std::size_t screen_width, const std::size_t screen_height,
+        const std::size_t grid_width, const std::size_t grid_height);
+    ~dohoa();
+    void ClearScreen();
+    void Dohoa(Snake const snake, SDL_Point const& food);
+private:
+    SDL_Window* sdl_window;
+    SDL_Renderer* sdl_renderer;
+    const std::size_t screen_width;
+    const std::size_t screen_height;
+    const std::size_t grid_width;
+    const std::size_t grid_height;
+};
+#endif
+
+
+
+
+
